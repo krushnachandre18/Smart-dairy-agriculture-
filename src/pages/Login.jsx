5410b3575874b492752942f4182d6840ad04cc99
Krushna chandre <krushnachandre5448@gmail.com>
2026-09-09 18:11:46 +0000

    create login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,38 @@
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate("/farmer/dashboard");
+  };
+
+  return (
+    <div>
+      <h1>Smart Dairy</h1>
+      <h2>Farmer Login</h2>
+
+      <input
+        type="text"
+        placeholder="Mobile Number"
+      />
+
+      <br />
+      <br />
+
+      <input
+        type="password"
+        placeholder="Password"
+      />
+
+      <br />
+      <br />
+
+      <button onClick={handleLogin}>
+        Login
+      </button>
+    </div>
+  );
+}
+
+export default Login;
